2670bb48578aa152b503885e9ec2ff8b727c0c25
Akash Kumar <akashkofficial54@gmail.com>
2022-07-23 08:14:01 +0530

    makes more cart item

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import CartItem from "./CartItem";
-
+// import CartItem from "./CartItem";
+import Cart from "./Cart";
 
 function App() {
   return (
     <div className="app">
-    <h1>Cart</h1>
-    <CartItem />
+    {/* <h1>Cart</h1> */}
+    <Cart />
     </div>
   );
 }
